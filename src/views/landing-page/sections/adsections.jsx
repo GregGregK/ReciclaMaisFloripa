6ba34aaf0f61febab7098133e21f4b62e7053e48
GregGregK <gregori_monteiro@estudante.sc.senai.br>
2024-07-01 19:44:16 -0300

    01-07:commit

--- src/views/landing-page/sections/adsections.jsx
+++ src/views/landing-page/sections/adsections.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { LoadScript, Autocomplete } from '@react-google-maps/api';
-import { useNavigate } from 'react-router-dom'; // Importe useNavigate ao invés de useHistory
+import { useNavigate } from 'react-router-dom';
 import '../../../assets/adsections.css';
 
 // Importe as imagens (adicione suas imagens ao diretório apropriado)
-import image1 from '../../../assets/img/eco1.png'; // Substitua pelo caminho correto da imagem
-import image2 from '../../../assets/img/eco2.png'; // Substitua pelo caminho correto da imagem
+import image1 from '../../../assets/img/eco1.png';
+import image2 from '../../../assets/img/eco2.png';
 
 const AdSections = () => {
-  const navigate = useNavigate(); // Use useNavigate ao invés de useHistory
+  const navigate = useNavigate();
   const [value, setValue] = useState('');
   const [autocomplete, setAutocomplete] = useState(null);
 
@@ -27,10 +27,12 @@ const AdSections = () => {
     }
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      navigate(`/alerta-coletas?bairro=${encodeURIComponent(value)}`); // Utilize navigate ao invés de history.push
-    }
+  const handleSearchClick = () => {
+    navigate(`/alerta-coletas?bairro=${encodeURIComponent(value)}`);
+  };
+
+  const handleCadastroClick = () => {
+    navigate('/cadastro-alerta');
   };
 
   return (
@@ -39,22 +41,23 @@ const AdSections = () => {
         <h2>Digite seu bairro e se informe dos dias de coleta</h2>
         <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} libraries={['places']}>
           <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-            <input
-              type="text"
-              placeholder="Digite o seu bairro"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              onKeyPress={handleKeyPress}
-            />
+            <div className="search-container">
+              <input
+                type="text"
+                placeholder="Digite o seu bairro"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+              <button onClick={handleSearchClick}>Pesquisar</button>
+            </div>
           </Autocomplete>
-          
         </LoadScript>
         <img src={image1} alt="Imagem relacionada ao anúncio 1" />
       </div>
       <div className="ad-section">
         <h2>Não perca mais o dia de coleta!</h2>
         <p>Cadastre seu número e receba uma mensagem avisando quando irão passar por sua rua!</p>
-        <button>Cadastrar</button>
+        <button onClick={handleCadastroClick}>Cadastrar</button>
         <img src={image2} alt="Imagem relacionada ao anúncio 2" />
       </div>
     </div>
